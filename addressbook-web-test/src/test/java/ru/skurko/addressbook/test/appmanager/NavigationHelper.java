64b7e5db1685778977dfd1df635c2f9e806a66d4
VictorSkurko <viktor.skurko@gmail.com>
2017-11-09 10:57:33 +0300

    проверка на нахождение на странице
    Если находимся на нужной странице, то переход выполнять не надо. NavigationHelper

--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/appmanager/NavigationHelper.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/appmanager/NavigationHelper.java
@@ -2,7 +2,6 @@ package ru.skurko.addressbook.test.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase{
 
@@ -10,7 +9,12 @@ public class NavigationHelper extends HelperBase{
         super(wd);
     }
 
-    public void goToGroupPage() {
+    public void goToGroupPage(){
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+            return;
+        }
         click(By.linkText("groups"));
     }
 }

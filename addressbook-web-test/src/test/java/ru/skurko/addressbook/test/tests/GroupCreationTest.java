19a14a82360ae8709f733df650c3e5830e952d61
VictorSkurko <viktor.skurko@gmail.com>
2017-11-23 11:16:07 +0300

    Реализовал Лямбда выражение для создания групп

--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/GroupCreationTest.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.skurko.addressbook.test.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,29 +16,30 @@ public class GroupCreationTest extends TestBase {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
 
-//        int before = app.getGroupHelper().getGroupCount();
-
-
         GroupData group = new GroupData(
 
-                "NewI",
+                "NewIV",
                 null,
                 null);
 
         app.getGroupHelper().createGroup(group);
 
         List<GroupData> after = app.getGroupHelper().getGroupList();
-//        int after = app.getGroupHelper().getGroupCount();
 
         Assert.assertEquals(after.size(), before.size()+1);
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+//        int max = 0;
+//        for (GroupData g : after) {
+//            if (g.getId() > max) {
+//                max = g.getId();
+//            }
+//        }
+
+//         Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(),o2.getId());
+
+//        int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
+
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
 
         before.add(group);
 

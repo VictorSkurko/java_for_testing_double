a44cb897347c0150ffa226d782227055a0d31396
VictorSkurko <viktor.skurko@gmail.com>
2017-11-26 17:31:03 +0300

    Revert "Revert "Доработал тест модификации группы""
    This reverts commit 46726b6a11a5895fc1eaa58d791628f69dd9d9b0.

--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/model/GroupData.java
@@ -1,11 +1,24 @@
 package ru.skurko.addressbook.test.model;
 
 public class GroupData {
+    private final String id;
     private final String groupName;
     private final String groupHeader;
     private final String groupFooter;
 
+    public String getId() {
+        return id;
+    }
+
     public GroupData(String groupName, String groupHeader, String groupFooter) {
+        this.id = null;
+        this.groupName = groupName;
+        this.groupHeader = groupHeader;
+        this.groupFooter = groupFooter;
+    }
+
+    public GroupData(String id, String groupName, String groupHeader, String groupFooter) {
+        this.id = id;
         this.groupName = groupName;
         this.groupHeader = groupHeader;
         this.groupFooter = groupFooter;
@@ -26,7 +39,8 @@ public class GroupData {
     @Override
     public String toString() {
         return "GroupData{" +
-                "groupName='" + groupName + '\'' +
+                "id='" + id + '\'' +
+                ", groupName='" + groupName + '\'' +
                 '}';
     }
 
@@ -37,11 +51,14 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
+        if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
         return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
     }
 
     @Override
     public int hashCode() {
-        return groupName != null ? groupName.hashCode() : 0;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
+        return result;
     }
 }

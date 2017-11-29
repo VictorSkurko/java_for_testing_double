ccdb5a69c587f5686b94ebcef5b6eaff9c7a0f3e
VictorSkurko <viktor.skurko@gmail.com>
2017-11-29 12:22:03 +0300

    4.10 сортировка с тесте модификации групп
            Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
            before.sort(byId);
            after.sort(byId);
    
            Assert.assertEquals(before,after);

--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/grouptests/GroupModifyTest.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/grouptests/GroupModifyTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.skurko.addressbook.test.model.GroupData;
 import ru.skurko.addressbook.test.tests.TestBase;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -43,6 +44,11 @@ public class GroupModifyTest extends TestBase {
 
         before.remove(before.size()-1);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after)); //Преобразуем списки во множества
+
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+
+        Assert.assertEquals(before,after);
     }
 }

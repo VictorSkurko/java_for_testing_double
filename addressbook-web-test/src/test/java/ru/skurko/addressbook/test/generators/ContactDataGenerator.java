4defa561223e891dfe3684552a349d8870776527
VictorSkurko <viktor.skurko@gmail.com>
2017-12-10 11:02:26 +0300

    6.9 Конфигурационные файлы описания тестовых наборов

--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/generators/ContactDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/generators/ContactDataGenerator.java
@@ -74,40 +74,42 @@ public class ContactDataGenerator {
         Gson gson = new GsonBuilder().setPrettyPrinting()
                 .excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        /* Здесь и далее применяем try для того,
+        чтобы не забыть закрыть файл вместо указания writer.close().
+        Файл закроется сам.*/
+        try (Writer writer = new FileWriter(file);) {
+            writer.write(json);
+        }
     }
 
     private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
         XStream xstream = new XStream();
         xstream.processAnnotations(ContactData.class);
         String xml = xstream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try (Writer writer = new FileWriter(file);) {
+            writer.write(xml);
+        }
     }
 
     private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
 
         System.out.println(new File(".").getAbsolutePath());
-
-        Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts) {
-            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
-                    contact.getFirstname(),
-                    contact.getMiddlename(),
-                    contact.getLastname(),
-                    contact.getNickname(),
+        try (Writer writer = new FileWriter(file);) {
+            for (ContactData contact : contacts) {
+                writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
+                        contact.getFirstname(),
+                        contact.getMiddlename(),
+                        contact.getLastname(),
+                        contact.getNickname(),
 //                    contact.getGroup(),
-                    contact.getAddress(),
-                    contact.getHomePhone(),
-                    contact.getMobilePhone(),
-                    contact.getWorkPhone(),
-                    contact.getEmail(),
-                    contact.getEmail2(),
-                    contact.getEmail3()));
+                        contact.getAddress(),
+                        contact.getHomePhone(),
+                        contact.getMobilePhone(),
+                        contact.getWorkPhone(),
+                        contact.getEmail(),
+                        contact.getEmail2(),
+                        contact.getEmail3()));
+            }
         }
-        writer.close();
     }
 }
--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/TestBase.java
@@ -7,7 +7,8 @@ import ru.skurko.addressbook.test.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app
+            = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeSuite
     public void setUp() throws Exception {

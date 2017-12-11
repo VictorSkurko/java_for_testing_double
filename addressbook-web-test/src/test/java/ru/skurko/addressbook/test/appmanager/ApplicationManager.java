8b1f5b11565a6b97eef72fd8803f39654f25a5cb
VictorSkurko <viktor.skurko@gmail.com>
2017-12-11 08:04:37 +0300

    Задание 14
    Задание №14: Вынести описание тестового стенда в конфигурационный файл
    Реализовать загрузку информации о тестовом стенде из конфигурационного файла.

--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/appmanager/ApplicationManager.java
@@ -8,10 +8,16 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private final Properties properties;
 
     //Здесь FirefoxDriver заменен на WebDriver т.к. это интерфейс с описанием
     // драйверов для различных браузеров. Теперь wd может принимать различные
@@ -25,12 +31,16 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser){
         this.browser = browser;
+        properties = new Properties();
     }
 
+    public void init() throws IOException {
 
-    public void init() {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader
+                (new File(String.format("src/test/resources/%s.properties", target))));
 
         //Проверяем тип браузера и присваиваем wd соответствующий тип
 
@@ -46,15 +56,14 @@ public class ApplicationManager {
 
         //Далее меняем FirefoxDriver на WebDriver в Helper-ах
 
+        wd.get(properties.getProperty("web.baseUrl"));
         navigationHelper = new NavigationHelper(wd);
-
         groupHelper = new GroupHelper(wd);
-
         contactHelper = new ContactHelper(wd);
-
         sessionHelper = new SessionHelper(wd);
-
-        sessionHelper.login("admin", "secret");
+        sessionHelper
+                .login(properties.getProperty("web.adminLogin"),
+                        properties.getProperty("web.adminPassword"));
     }
 
     public void alertOk() {

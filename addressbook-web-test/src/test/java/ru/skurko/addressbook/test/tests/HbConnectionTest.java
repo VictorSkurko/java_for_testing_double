06dd5cbe96eed5839b76756894fbdd5eb1c4276c
VictorSkurko <viktor.skurko@gmail.com>
2017-12-13 08:51:10 +0300

    Лекция7.2
    Выводим информацию о группах из БД и Фильтруем информацию из лога по уровню.

--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/HbConnectionTest.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/HbConnectionTest.java
@@ -8,7 +8,6 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.skurko.addressbook.test.model.GroupData;
-
 import java.util.List;
 
 

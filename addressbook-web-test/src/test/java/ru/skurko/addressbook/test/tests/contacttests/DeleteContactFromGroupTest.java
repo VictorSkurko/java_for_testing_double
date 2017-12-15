d4ee617b72aa17c5ba8fcfeb320b558286344938
VictorSkurko <viktor.skurko@gmail.com>
2017-12-15 11:10:00 +0300

    Работа над удалением контакта из группы

--- addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/contacttests/DeleteContactFromGroupTest.java
+++ addressbook-web-test/src/test/java/ru/skurko/addressbook/test/tests/contacttests/DeleteContactFromGroupTest.java
@@ -9,22 +9,21 @@ import ru.skurko.addressbook.test.model.GroupData;
 import ru.skurko.addressbook.test.tests.TestBase;
 
 import java.util.Iterator;
-import static org.testng.Assert.assertEquals;
 
-public class AddContactToGroupTest extends TestBase {
+public class DeleteContactFromGroupTest extends TestBase {
     @BeforeMethod
     public void ensurePreconditions() {
-        if (app.db().contacts().size() == 0) {
-            createContact();
-        }
         if (app.db().groups().size() == 0) {
             app.goTo().groupPage();
             app.group().create(new GroupData().withGroupName("NewI")
                     .withGroupHeader("NewI Header").withGroupFooter("NewI Footer"));
         }
+        if (app.db().contacts().size() == 0) {
+            createContact();
+        }
     }
 
-    private void createContact() {
+    private ContactData createContact() {
         app.goTo().contactPage();
         ContactData contactData = new ContactData()
                 .withFirstName("Владимир")
@@ -35,40 +34,43 @@ public class AddContactToGroupTest extends TestBase {
                 .withWorkPhone("66-666-6")
                 .withEmail("mail@mail.ru");
         app.contact().create(contactData);
+        return contactData;
     }
 
     @Test
     public void testAddContactToGroup(){
-        Contacts contacts = app.db().contacts();
         Groups groups = app.db().groups();
-        ContactData contact;
+        Contacts contacts = app.db().contacts();
         GroupData group = null;
-        boolean foundContactNotInGroup = false;
-
+        ContactData contact;
+        boolean foundContactInGroup = false;
         Iterator<ContactData> contactsIterator = contacts.iterator();
         while ((contact = contactsIterator.next()) != null) {
             Iterator<GroupData> groupsIterator = groups.iterator();
             while((group = groupsIterator.next()) != null) {
-                if (!contact.getGroups().contains(group)){
-                    foundContactNotInGroup = true;
+                if (contact.getGroups().contains(group)){
+                    foundContactInGroup = true;
                     break;
                 }
             }
-            if(foundContactNotInGroup){
+            if(foundContactInGroup){
                 break;
             }
         }
 
-        if(!foundContactNotInGroup) {
-            createContact();
-        }
         group = group != null ? group : groups.iterator().next();
+        if(!foundContactInGroup) {
+            contact = createContact();
+            app.goTo().contactPage();
+            app.contact().addToGroup(contact, group);
+        }
         app.goTo().contactPage();
-        app.contact().addToGroup(contact, group);
+        app.contact().removeFromGroup(contact, group);
         ContactData finalContact = contact;
         ContactData contactFromDb = app.db().contacts().stream()
                 .filter(c -> c.getId() == finalContact.getId())
                 .findFirst().orElse(null);
-        assertEquals(true, contactFromDb.getGroups().contains(group));
+        assertEquals(false, contactFromDb.getGroups().contains(group));
+
     }
 }

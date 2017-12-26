eee3de9df9a4fef7d8f09d9eaeb20b15e8b32310
VictorSkurko <viktor.skurko@gmail.com>
2017-12-26 08:03:58 +0300

    9.2 Создали модуль soap-example
    Подключили плагин, создали тест для проверки IP адреса

---
+++ soap-example/src/test/java/ru/skurko/javatesting/soap/GeoIpServiceTest.java
@@ -0,0 +1,17 @@
+package ru.skurko.javatesting.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTest {
+
+    @Test
+    public void testMyIp() {
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("87.117.54.73");
+        assertEquals(geoIP.getCountryCode(), "RUS");
+    }
+}
